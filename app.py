ce8ced78788fa5241598056737ccedd037f74bb3
Blair <stephenjohnblair@outlook.com>
2019-05-31 10:53:07 +0000

    Linked app.py to ports

--- app.py
+++ app.py
@@ -3,10 +3,21 @@ from flask import Flask, render_template, redirect, request, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-app = Flask(__name__)
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'task_manager'
+
+app.config["MONGO_DBNAME"] = 'cookbook'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:r00tUser@myfirstcluster-lq5f8.mongodb.net/cookbook?retryWrites=true')
 
 mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_cuisines')
+def get_cuisines():
+    return render_template("cuisines.html", 
+                           cuisines=mongo.db.cuisines.find())
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
